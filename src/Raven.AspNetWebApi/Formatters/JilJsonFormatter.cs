975331d6f741e39ec1c323e95b66d1e1633c2fc7
liangyi <liangyi@mallcoo.cn>
2015-11-05 16:18:25 +0800

    111

---
+++ src/Raven.AspNetWebApi/Formatters/JilJsonFormatter.cs
@@ -0,0 +1,76 @@
+﻿using Jil;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raven.AspNetWebApi.Formatter
+{
+    public class JilJsonFormatter : MediaTypeFormatter
+    {
+        private readonly Options _jilOptions;
+        public JilJsonFormatter()
+        {
+            _jilOptions = new Options(dateFormat: DateTimeFormat.ISO8601);
+
+            SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/json"));
+
+            SupportedEncodings.Add(new UTF8Encoding(encoderShouldEmitUTF8Identifier: false, throwOnInvalidBytes: true));
+            SupportedEncodings.Add(new UnicodeEncoding(bigEndian: false, byteOrderMark: true, throwOnInvalidBytes: true));
+        }
+        public override bool CanReadType(Type type)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException("type");
+            }
+            return true;
+        }
+
+        public override bool CanWriteType(Type type)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException("type");
+            }
+            return true;
+        }
+
+        public override Task<object> ReadFromStreamAsync(Type type, Stream readStream, System.Net.Http.HttpContent content, IFormatterLogger formatterLogger)
+        {
+            return Task.FromResult(this.DeserializeFromStream(type, readStream));
+        }
+
+
+        private object DeserializeFromStream(Type type, Stream readStream)
+        {
+
+            try
+            {
+                using (var reader = new StreamReader(readStream))
+                {
+                    return JSON.Deserialize(reader, type, _jilOptions);
+                }
+            }
+            catch
+            {
+                return null;
+            }
+
+        }
+
+        public override Task WriteToStreamAsync(Type type, object value, Stream writeStream, System.Net.Http.HttpContent content, TransportContext transportContext)
+        {
+            var streamWriter = new StreamWriter(writeStream);
+            JSON.Serialize(value, streamWriter, _jilOptions);
+            streamWriter.Flush();
+            return Task.FromResult(writeStream);
+        }
+    }
+}
---
+++ src/Raven.AspNetWebApi/Formatters/MsgPackFormatter.cs
@@ -0,0 +1,49 @@
+﻿using MsgPack.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Formatting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raven.AspNetWebApi.Formatters
+{
+    public class MsgPackFormatter : MediaTypeFormatter
+    {
+        public override bool CanReadType(Type type)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException("type");
+            }
+            return true;
+        }
+
+        public override bool CanWriteType(Type type)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException("type");
+            }
+            return true;
+        }
+
+
+        public override Task<object> ReadFromStreamAsync(Type type, Stream readStream, System.Net.Http.HttpContent content, IFormatterLogger formatterLogger)
+        {
+            var serializer = SerializationContext.Default.GetSerializer(type);
+            var obj = serializer.Unpack(readStream);
+            return Task.FromResult(obj);
+        }
+        
+
+        public override Task WriteToStreamAsync(Type type, object value, Stream writeStream, System.Net.Http.HttpContent content, TransportContext transportContext)
+        {
+            var serializer = SerializationContext.Default.GetSerializer(type);
+            serializer.Pack(writeStream, value);
+            return writeStream.FlushAsync();
+        }
+    }
+}
