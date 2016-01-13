889daf81e5d6a0a94facd099cb15bad944eff60a
Indifer <Indifer@126.com>
2016-01-13 11:17:18 +0800

    1

--- src/Raven.AspNet/Raven.AspNet.MvcExtensions/View/CustomWebViewPage.cs
+++ src/Raven.AspNet/Raven.AspNet.MvcExtensions/View/CustomWebViewPage.cs
@@ -21,6 +21,14 @@ namespace Raven.AspNet.MvcExtensions.View
         /// 资源是否压缩
         /// </summary>
         protected abstract bool IsAssetsCompress { get; }
+        
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public CustomWebViewPage()
+        {
+        }
+        
 
         /// <summary>
         /// RenderScript
@@ -57,6 +65,42 @@ namespace Raven.AspNet.MvcExtensions.View
 
         }
 
+
+        /// <summary>
+        /// ScriptUrl
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public virtual string ScriptUrl(string path)
+        {
+            var bundlesPath = path.ToLower().Replace(".js", "").Replace(".", "_");//.Replace("assets", "bundles");
+            var bundles = System.Web.Optimization.BundleTable.Bundles;
+
+            if (!_scriptsHs.Contains(path))
+            {
+                lock (_scriptsHs)
+                {
+                    if (!_scriptsHs.Contains(path) && !bundles.Any(m => m.Path == path))
+                    {
+                        bundles.Add(new System.Web.Optimization.ScriptBundle(bundlesPath).Include(path));
+                        _scriptsHs.Add(path);
+                    }
+                }
+            }
+
+
+            //是否启用资源压缩
+            if (IsAssetsCompress)
+            {
+                return System.Web.Optimization.Scripts.Url(bundlesPath).ToString();
+            }
+            else
+            {
+                return System.Web.Optimization.Scripts.Url(path).ToString();
+            }
+
+        }
+
         /// <summary>
         /// RenderStyle
         /// </summary>
@@ -87,6 +131,40 @@ namespace Raven.AspNet.MvcExtensions.View
             else
             {
                 return System.Web.Optimization.Styles.Render(path);
+            }
+
+        }
+
+        /// <summary>
+        /// StyleUrl
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public virtual string StyleUrl(string path)
+        {
+            var bundlesPath = path.ToLower().Replace(".css", "").Replace(".", "_");//.Replace("assets", "bundles");
+            var bundles = System.Web.Optimization.BundleTable.Bundles;
+
+            if (!_stylesHs.Contains(path))
+            {
+                lock (_stylesHs)
+                {
+                    if (!_stylesHs.Contains(path) && !bundles.Any(m => m.Path == path))
+                    {
+                        bundles.Add(new System.Web.Optimization.StyleBundle(bundlesPath).Include(path));
+                        _stylesHs.Add(path);
+                    }
+                }
+            }
+
+            //是否启用资源压缩
+            if (IsAssetsCompress)
+            {
+                return System.Web.Optimization.Styles.Url(bundlesPath).ToString();
+            }
+            else
+            {
+                return System.Web.Optimization.Styles.Url(path).ToString();
             }
 
         }
