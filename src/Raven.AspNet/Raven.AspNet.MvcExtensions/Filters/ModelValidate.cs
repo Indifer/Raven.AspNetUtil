650154be1471372934646ac79c4c4455a974fa69
MingHao <MingHao@mallcoo.cn>
2017-10-30 17:21:12 +0800

    添加模型验证过滤器

---
+++ src/Raven.AspNet/Raven.AspNet.MvcExtensions/Filters/ModelValidate.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Raven.AspNet.MvcExtensions.Filters
+{
+    /// <summary>
+    /// 模型验证过滤器
+    /// </summary>
+    public class ModelValidateAttribute : ActionFilterAttribute
+    {
+        /// <summary>
+        /// 验证失败时跳转的uri
+        /// 例如：https://m.mallcoo.com.cn/error?msg={0}
+        /// </summary>
+        public string ErrorUri { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="errorUri">验证失败时跳转的uri例如：https://m.mallcoo.com.cn/error?msg={0} </param>
+        public ModelValidateAttribute(string errorUri )
+        {
+            ErrorUri = errorUri;
+        }
+
+        /// <summary>
+        /// Action执行前进行模型验证
+        /// </summary>
+        /// <param name="filterContext"></param>
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var modelState = filterContext.Controller.ViewData.ModelState;
+            if (!modelState.IsValid)
+            {
+                StringBuilder errMsg = new StringBuilder();
+                foreach (var item in modelState.Values)
+                {
+                    errMsg.AppendLine(string.Join(";", item.Errors.Select(e => e.ErrorMessage)));
+                }
+                filterContext.Result = new RedirectResult(string.Format(ErrorUri,errMsg));
+            }
+        }
+
+        
+    }
+}
---
+++ src/Raven.AspNet/Raven.AspNet.WebApiExtensions/Filters/ModelValidateFilter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace Raven.AspNet.WebApiExtensions.Filters
+{
+    /// <summary>
+    /// 模型验证过滤器
+    /// </summary>
+    public class ModelValidateAttribute:ActionFilterAttribute
+    {
+        /// <summary>
+        /// 验证失败错误码
+        /// </summary>
+        const int ValidateFailedCode = 400;
+        /// <summary>
+        /// Action执行前进行模型验证
+        /// </summary>
+        /// <param name="actionContext"></param>
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            if (!actionContext.ModelState.IsValid)
+            {
+                StringBuilder errMsg=new StringBuilder();
+                foreach (var item in actionContext.ModelState.Values)
+                {
+                    errMsg.AppendLine(string.Join(";", item.Errors.Select(e => e.ErrorMessage)));
+                }
+                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.OK,new
+                {
+                    Code=ValidateFailedCode,
+                    Message=errMsg.ToString()
+                });
+            }
+        }
+    }
+}
